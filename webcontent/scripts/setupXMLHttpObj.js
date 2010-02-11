403caea359a46b6a16ea5aa9bd7b9cdc6085d60c
LudicrousResearcher@gmail.com <pschmitz@users.noreply.github.com>
2010-02-11 06:43:34 +0000

    Adding to repo

---
+++ webcontent/scripts/setupXMLHttpObj.js
@@ -0,0 +1,33 @@
+// This should only be included once in a given HTML file, as it sets up the
+// request object for that file.
+var xmlhttp=false;
+
+/*@cc_on @*/
+/*@if (@_jscript_version >= 5)
+// JScript gives us Conditional compilation, we can cope with old IE versions.
+// and security blocked creation of the objects.
+ try {
+  xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+ } catch (e) {
+  try {
+   xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  } catch (E) {
+   xmlhttp = false;
+  }
+ }
+@end @*/
+if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+	try {
+		xmlhttp = new XMLHttpRequest();
+	} catch (e) {
+		xmlhttp=false;
+	}
+}
+if (!xmlhttp && window.createRequest) {
+	try {
+		xmlhttp = window.createRequest();
+	} catch (e) {
+		xmlhttp=false;
+	}
+}
+
