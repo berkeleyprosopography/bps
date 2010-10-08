e55f373182e80ee7c026c8ee70692cb9a9046595
LudicrousResearcher@gmail.com <pschmitz@users.noreply.github.com>
2010-10-08 20:03:53 +0000

    Updated params UI again. Made basic params php handle crude versioning of template.

--- webcontent/modules/corpora/params.php
+++ webcontent/modules/corpora/params.php
@@ -2,7 +2,16 @@
 
 require_once("../../libs/env.php");
 
+$tempToUse = 'corpora_params.tpl';
+if( isset( $_GET['v'] ) ) {
+	$variant = $_GET['v'];
+	if($variant=='2') {
+		$tempToUse = 'corpora_params2.tpl';
+	} else if($variant=='3') {
+		$tempToUse = 'corpora_params3.tpl';
+	}
+} 
 // Display template
-$t->display('corpora_params.tpl');
+$t->display($tempToUse);
 
 ?>
