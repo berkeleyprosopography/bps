feb3aedd1ab98bdea02c1f27583be932b902c5b8
LudicrousResearcher@gmail.com <LudicrousResearcher@gmail.com@b00db2cd-2a52-0410-a7b9-6ba78f1a8ee0>
2010-10-12 00:44:48 +0000

    Moving over files to new structure.

---
+++ trunk/code/bps/services/common/src/main/java/edu/berkeley/bps/services/time/BaseTimeSpan.java
@@ -0,0 +1,29 @@
+package edu.berkeley.bps.services.time;
+
+public abstract class BaseTimeSpan implements TimeSpan {
+
+	protected double stdDev = 0;
+	private double twoVariance = 0;
+
+	public BaseTimeSpan(double stdDev) {
+		super();
+		setStdDev(stdDev);
+	}
+
+	public double getStdDev() {
+		return stdDev;
+	}
+
+	public void setStdDev(double value) {
+		if(value<=0)
+			throw new IllegalArgumentException("TimeSpace StdDev must be > 0");
+		stdDev = value;
+		twoVariance = 2*stdDev*stdDev;
+	}
+
+	public double computeProbabilityForTime(long time) {
+		long delta = (time-getCenterPoint());
+		return Math.pow(Math.E, (-delta*delta/twoVariance));
+	}
+
+}
