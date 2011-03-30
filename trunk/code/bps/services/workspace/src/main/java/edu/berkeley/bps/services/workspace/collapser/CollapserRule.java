a2c8223776a59d2cf7e67a141a6cb76ed19d8512
LudicrousResearcher@gmail.com <LudicrousResearcher@gmail.com@b00db2cd-2a52-0410-a7b9-6ba78f1a8ee0>
2011-03-30 20:55:15 +0000

    Advances to rule classes.

--- trunk/code/bps/services/workspace/src/main/java/edu/berkeley/bps/services/workspace/collapser/CollapserRule.java
+++ trunk/code/bps/services/workspace/src/main/java/edu/berkeley/bps/services/workspace/collapser/CollapserRule.java
@@ -4,13 +4,18 @@ import edu.berkeley.bps.services.workspace.Entity;
 
 public interface CollapserRule {
 
+	public static final int SHIFT_RULE = 1;
+	public static final int DISCOUNT_RULE = 2;
+	public static final int BOOST_RULE = 3;
+	
 	/**
 	 * Evaluates collapsing (shifting weight) from fromEntity into toEntity.
 	 * Will scale any evaluation based upon the current value of weight.
 	 * @param first First entity to consider
 	 * @param second Second entity to consider
 	 * @return 0 if this rule does not apply to the pair,
-	 * 			else a value that depends upon the class of rule 
+	 * 			else a value that depends upon the type of rule
+	 * 			SHIFT rules must return a value from -1 to 1 
 	 */
 	public float evaluate(Entity fromEntity, Entity toEntity);
 
@@ -28,15 +33,30 @@ public interface CollapserRule {
 	/**
 	 * @return the current weight to give this rule.
 	 */
-	public float getWeight();
+	public double getWeight();
 	
 	/**
 	 * Sets the current weight to give this rule. Set to 0 to disable the rule, 
 	 * set to 1 for complete confidence in the rule, or some value between to
 	 * correspond with the confidence or weight to give this rule.
-	 * @param weight must be in the range of 0 to 1
-	 * @throws IllegalArgumentException if weight < 0 or weight > 1
+	 * @param weight must be greater than 0
+	 * @throws IllegalArgumentException if weight < 0
 	 */
-	public void setWeight(float weight);
+	public void setWeight(double weight);
+	
+	/**
+	 * @return the defined type for this rule - one of SHIFT_RULE, DISCOUNT_RULE, BOOST_RULE
+	 */
+	public int getType();
+
+	/**
+	 * @return true, if this rule applies within documents
+	 */
+	public boolean appliesWithinDocument();
+	
+	/**
+	 * @return true, if this rule applies across documents in a corpus
+	 */
+	public boolean appliesAcrossCorpus();
 	
 }
