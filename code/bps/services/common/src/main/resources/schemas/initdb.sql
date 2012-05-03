da0e604ce881299c25f5346663b1740e49c96d5f
LudicrousResearcher@gmail.com <pschmitz@users.noreply.github.com>
2012-05-03 22:36:27 +0000

    Cleaning up from previous checkin.

--- code/bps/services/common/src/main/resources/schemas/initdb.sql
+++
@@ -1,9 +0,0 @@
--- ----------------------------------------------------------------------
--- SQL create script for BPS main object info tables
--- ----------------------------------------------------------------------
-
--- Will create with default collation of 'utf8_general_ci'
-DROP database IF EXISTS @DB_NAME@;
-CREATE DATABASE @DB_NAME@ DEFAULT CHARACTER SET utf8;
-
-
